e751b864eca4c55cc0d6c34bb93833728f55e2de
RodrigoMontoya <rodrigomontoya1996@gmail.com>
2023-02-25 19:21:02 -0300

    Primeros cambios en html y css

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[2].classList.add("GitGitHub");
+        habilidades[3].classList.add("MySql");
+        habilidades[4].classList.add("Java");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
